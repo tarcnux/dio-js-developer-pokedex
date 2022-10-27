3a4bacc259034ca97841ddf6fd6431dd42be1e85
Tarcisio Nunes <github@tarcnux.com.br>
2022-10-27 19:20:54 -0300

    closes #17

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,5 @@ console.log("main.js importado corretamente");
 console.log("API: https://pokeapi.co/api/v2/pokemon/");
 console.log("Protocolo HTTP");
 console.log("URL Path e Method");
-console.log("16 - Path Params E Query String")
+console.log("16 - Path Params E Query String");
+console.log("17 - Headers");
