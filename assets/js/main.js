7c8059ea5ec9cccf5ac1baffd29f232753714ac3
Tarcisio Nunes <github@tarcnux.com.br>
2022-10-27 20:49:16 -0300

    closes #20

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,15 +5,11 @@ let limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
 fetch(url)
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
-    .finally(function() {
-        console.log('Requisição concluída.')
-    });
+    .then(response =>  response.json())
+    .then(jsonBody => console.log(jsonBody))
+    .catch(error  =>  console.log(error))
+    .finally(() => console.log('Requisição concluída.'))
+
 const x = 10 + 10;
 
 console.log(x);
