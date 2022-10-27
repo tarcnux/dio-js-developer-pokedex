81200cd3ceafdf1f9ab494e81232720a6dd13011
Tarcisio Nunes <github@tarcnux.com.br>
2022-10-27 20:09:25 -0300

    Promise then - catch - finally

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,12 @@ const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
 fetch(url)
     .then(function (response) {
         console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .finally(function() {
+        console.log('Requisição concluída.')
     });
 const x = 10 + 10;
 
